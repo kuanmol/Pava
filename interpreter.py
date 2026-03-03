72cffb8d95c776b4dbd24c8ea951c4c564f47ade
kuanmol <ramuklomna100@gmail.com>
2026-03-03 23:48:48 +0530

    Add basic interpreter: variables, print, and simple math execution

---
+++ interpreter.py
@@ -0,0 +1,32 @@
+from node import NumberNode, VarAssignNode, VarAccessNode, PrintNode
+
+
+class Interpreter:
+    def __init__(self):
+        self.variables = {}
+
+    def visit(self, node):
+        method_name = f"visit_{type(node).__name__}"
+        method = getattr(self, method_name, self.no_visit_method)
+        return method(node)
+
+    def no_visit_method(self, node):
+        raise Exception(f"No visit_{type(node).__name__} method define")
+
+    def visit_NumberNode(self, node):
+        return node.value
+
+    def visit_VarAssignNode(self, node):
+        value = self.visit(node.value)
+        self.variables[node.name] = value
+        return value
+
+    def visit_VarAccessNode(self, node):
+        if node.name not in self.variables:
+            raise Exception(f"Variable {node.name} does not exist")
+        return self.variables[node.name]
+
+    def visit_PrintNode(self, node):
+        value = self.visit(node.value)
+        print(value)
+        return value
